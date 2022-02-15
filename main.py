92c3ddecfe93ac2284052466d5c98cfaffed23f3
augustus <a.matheus26@hotmail.com>
2022-02-15 15:13:24 -0300

    Added Threaded and Serial. Optional args go to tqdm

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
 from nornir import InitNornir
 from nornir.core.plugins.runners import RunnersPluginRegister
-from plugins.runners.progressbar import ProgressBarRunner
+from plugins.runners.progressbar import ThreadedProgressBarRunner, SerialProgressBarRunner
 from nornir.core.plugins.inventory import InventoryPluginRegister
 from nornir_csv.plugins.inventory import CsvInventory
 from nornir.core.task import Task, Result
 from time import sleep
-from os import urandom
-from quantumrandom import randint
+from random import random
 
 def hello_world(task: Task) -> Result:
-  sleep(5)
+  sleep(random()*10)
   return Result(
     result=f"Hello {task.host}",
     host=task.host,
     changed=False,)
 
-RunnersPluginRegister.register("ProgressBar", ProgressBarRunner)
+RunnersPluginRegister.register("ProgressBar", ThreadedProgressBarRunner)
 InventoryPluginRegister.register("CsvInventory", CsvInventory)
 
 nr = InitNornir(config_file="config.yaml")
--- plugins/runners/progressbar.py
+++ plugins/runners/progressbar.py
@@ -1,25 +1,52 @@
 from typing import Any, List, Type
 from concurrent.futures import ThreadPoolExecutor, as_completed
-from nornir.plugins.runners import ThreadedRunner
+from nornir.plugins.runners import ThreadedRunner, SerialRunner
 from nornir.core.task import Task, AggregatedResult
 from nornir.core.inventory import Host
 from tqdm import tqdm
-import pdb
 
-class ProgressBarRunner(ThreadedRunner):
+class SerialProgressBarRunner(SerialRunner):
     """
-    ProgressBarRunner runs the task over each host using threads.
+    SerialProgressBarRunner runs the task over each host sequentially.
+    Prints a cute progress bar.
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+        # Optional arguments are passed to tqdm
+        self.args = args
+        self.kwargs = kwargs
+
+    def run(self, task: Task, hosts: List[Host]) -> AggregatedResult:
+        result = AggregatedResult(task.name)
+        for host in tqdm(hosts,*self.args,**self.kwargs):
+            result[host.name] = task.copy().start(host)
+        return result
+
+class ThreadedProgressBarRunner(ThreadedRunner):
+    """
+    ThreadedProgressBarRunner runs the task over each host using threads.
     Prints a cute progress bar.
     Arguments:
         num_workers: number of threads to use
     """
+    def __init__(self, num_workers: int = 20, *args, **kwargs):
+        super().__init__(num_workers)
+        # Optional arguments are passed to tqdm
+        self.args = args
+        self.kwargs = kwargs
 
     def run(self, task: Task, hosts: List[Host]) -> AggregatedResult:
         result = AggregatedResult(task.name)
-        pdb.set_trace()
-        with ThreadPoolExecutor(self.num_workers) as executor:
-            executions = executor.map(task.copy().start, hosts)
-            pdb.set_trace()
-            for index, execution in enumerate(executions):
-                result[hosts[index].name] = execution
+        futures = []
+        with ThreadPoolExecutor(self.num_workers) as pool:
+            for host in hosts:
+                future = pool.submit(task.copy().start, host)
+                futures.append(future)
+
+            for future in tqdm(as_completed(futures),
+                               total=len(hosts),
+                               *self.args,**self.kwargs):
+                worker_result = future.result()
+                result[worker_result.host.name] = worker_result
+
         return result
